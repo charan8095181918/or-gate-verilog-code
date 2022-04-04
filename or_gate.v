1734bc6ed89bf35d5dca883fe53e4492d30a198e
charan8095181918 <60030130+charan8095181918@users.noreply.github.com>
2022-04-04 12:46:46 +0530

    Add files via upload

---
+++ or_gate.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+
+// Design Name: 
+// Module Name:    or_gate 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module and_gate(a,b,c);
+input a,b;
+output c;
+assign c=a|b;
+endmodule
---
+++ or_gate_test.v
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   12:32:59 04/04/2022
+// Design Name:   Car_Parking2
+// Module Name:   D:/exp/dsd/Charan/or_gate_test.v
+// Project Name:  Charan
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: Car_Parking2
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module or_gate_test();
+reg a,b;
+wire c;
+or_gate DUT(.a(a),.b(b),.c(c));
+initial begin
+$monitor("a=%b b=%b c=%b",a,b,c);
+a=0;
+b=0;
+#10
+a=0;
+b=1;
+#10
+a=1;
+b=0;
+#10
+a=1;
+b=1;
+#10;
+end
+endmodule
+
+module or_gate(a,b,c);
+input a,b;
+output c;
+assign c=a|b;
+endmodule
